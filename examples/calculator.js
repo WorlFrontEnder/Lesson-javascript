4528efb30f80619145951691dd7395b17807da28
WorlFrontEnder <Cyberverstalshik@yandex.ru>
2018-07-16 20:06:54 +0400

    Инициализация переменных в calculator.js

---
+++ examples/calculator.js
@@ -0,0 +1,9 @@
+// Поиск кнопок 
+var input = document.getElementById('input');  // найти циферблат
+var number = document.querySelectorAll('.numbers div') // найти все цифры кнопки
+var operator = document.querySelectorAll('operators div');  // найти все кнопки +,-,*,/,=
+var result = document.getElementById('result'); // кнопка равно
+var clear = document.getElementById('clear'); // кнопка обнуления 
+var resultDisplayed = false;
+
+
