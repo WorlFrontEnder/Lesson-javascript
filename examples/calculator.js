1f0180c31a5493b993246dbcaddff884bc7d1d82
WorlFrontEnder <Cyberverstalshik@yandex.ru>
2018-07-16 20:17:03 +0400

    Инициализация кнопок

--- examples/calculator.js
+++ examples/calculator.js
@@ -1,3 +1,7 @@
-
-
-
+	// Инициализация поиска элементов кнопок
+	var input = document.getElementById('input');
+	var number = document.querySelectorAll('numbers div');
+	var operator = document.querySelectorAll('operators div');
+	var result = document.getElementById('result');
+	var clear = document.getElementById('clear');
+    var resultDisplayed = false;
